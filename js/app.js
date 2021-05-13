40c6648f7a4d3f93a1eeacd57aae9b2ccd06d78c
Alli samuel <samueltemiloluwaalli@gmail.com>
2021-05-13 10:05:43 +0100

    added drag and drop

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
     const deleteButton = document.querySelector('.button-red');
     let id = 0;
     let tasks;
+    let showDate = false;
     
 
     function addQueueFunction    ()  {
@@ -27,8 +28,7 @@
             dropOffLoc.value ="";
             console.log(queue);
             onAdd();
-            setDate();
-            secondTableUpdate();
+            return !showDate? setDate(): null;
         }
            
     }
@@ -46,6 +46,9 @@
                    <span>${tasks.pickupLocation}</span>
                    <span>${tasks.dropOffLocation}</span>
                    `;
+                   listItem.setAttribute("draggable", "true");
+                   listItem.setAttribute("ondragstart", "drag(event)");
+                   listItem.setAttribute("id", "drag1");
                    list.append(listItem);
                 //    if statement for the other table
                }
@@ -69,133 +72,19 @@
         fifthDay.innerHTML=`${new Date(date.setDate(date.getDate() + 1)).toDateString()}`;
         sixthDay.innerHTML=`${new Date(date.setDate(date.getDate() + 1)).toDateString()}`;
         seventhDay.innerHTML = `${new Date(date.setDate(date.getDate() + 1)).toDateString()}`; 
-        days.className.add('.table-head');
     }
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
     
-
-    function secondTableUpdate    ()  {
-        const cell = document.querySelectorAll('.one');
-        const[
-            one,
-            two,
-            three,
-            four,
-            five,
-            six,
-            seven,
-            eight,
-            nine,
-            ten,
-            eleven,
-            twelve,
-            thirteen,
-            fourteen,
-            fifteen,
-            sixteen,
-            seventeen,
-            eighteen,
-            nineteen,
-            twenty,
-            twentyOne,
-            twentyTwo,
-            twentyThree,
-            twentyFour,
-            twentyFive,
-            twentySix,
-            twentySeven,
-            TwentyEight
-
-        ]= cell;
-        // creates new cell
-        // updates new cell
-        // moves to the next column when it exceed 4 rows
-        for(let i= id; i<=queue.length; i++)  {
-            if(i==1) {
-               return one.innerHTML = `${tasks.customerName}`; 
-            }
-            else if(i==2)   {
-                return eight.innerHTML = `${tasks.customerName}`; 
-            }
-            else if(i==3){
-                return fifteen.innerHTML = `${tasks.customerName}`; 
-            }
-            else if(i==4)  {
-                return twentyTwo.innerHTML = `${tasks.customerName}`; 
-            }
-            else if(i==5)  {
-                return two.innerHTML = `${tasks.customerName}`; 
-            }
-            else if(i==6){
-                return nine.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==7){
-                return sixteen.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==8){
-                return twentyThree.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==9){
-                return three.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==10){
-                return ten.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==11){
-                return seventeen.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==12){
-                return  twentyFour.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==13){
-                return  four.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==14){
-                return  eleven.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==15){
-                return  eighteen.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==16){
-                return  twentyFive.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==17){
-                return  five.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==18){
-                return  twelve.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==19){
-                return  nineteen.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==20){
-                return  twentySix.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==21){
-                return  six.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==22){
-                return  thirteen.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==23){
-                return twenty.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==24){
-                return twentySeven.innerHTML = `${tasks.customerName}`; 
-        }
-            else if(i==25){
-                return  seven.innerHTML = `${tasks.customerName}`; 
-        }
-        else if(i==26){
-            return  fourteen.innerHTML = `${tasks.customerName}`; 
-
-        }
-        else if (i==27)   {
-            return twentyOne.innerHTML = `${tasks.customerName}`; 
-        }
-        else{
-            return  TwentyEight.innerHTML = `${tasks.customerName}`; 
-        }
-    }   
-    }
 addButton.addEventListener('click', addQueueFunction);
-{/* <span><input type = "checkbox" id="${tasks.id}"/></span> */}
